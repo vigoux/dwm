d3e2a421054f6474d2f085d94362729437880834
Thomas Vigouroux <tomvig38@gmail.com>
2020-04-26 14:11:49 +0200

    config: dracula colors ans sane keys

--- config.def.h
+++ config.def.h
@@ -1,21 +1,19 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "monospace:size=20" };
+static const char dracula_fg[] = "#f8f8f2";
+static const char dracula_bg[] = "#282a36";
+static const char dracula_purple[] = "#bd93f9";
+static const char dracula_line[] = "#44475a";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { dracula_fg , dracula_bg, dracula_line },
+	[SchemeSel]  = { dracula_bg, dracula_purple,  dracula_purple  },
 };
 
 /* tagging */
@@ -44,7 +42,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -53,37 +51,41 @@ static const Layout layouts[] = {
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define OPENTERMAPP(app) { "st", "-e", app, NULL }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
+static const char *termcmd[]  = { "st", "-e", "/bin/fish", NULL };
+static const char *todocmd[] = OPENTERMAPP("/bin/calcurse");
+static const char *mailcmd[] = OPENTERMAPP("/bin/neomutt");
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	/* modifier						key			function		argument */
+	{ MODKEY,						XK_p,		spawn,			{.v = dmenucmd } },
+	{ 0, 							XK_Menu,	spawn,			{.v = dmenucmd } },
+	{ MODKEY,						XK_Return, spawn,			{.v = termcmd } },
+	{ MODKEY,						XK_b,      togglebar,      {0} },
+	{ MODKEY,						XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,						XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,						XK_l,      incnmaster,     {.i = +1 } },
+	{ MODKEY,						XK_h,      incnmaster,     {.i = -1 } },
+	{ MODKEY,						XK_d,      setmfact,       {.f = -0.05} },
+	{ MODKEY,						XK_i,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,				XK_Return, zoom,           {0} },
+	{ MODKEY,						XK_Tab,    view,           {0} },
+	{ MODKEY,						XK_x,      killclient,     {0} },
+	{ MODKEY,						XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,						XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,						XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,						XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,				XK_space,  togglefloating, {0} },
+	{ MODKEY,						XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,				XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,						XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,						XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,				XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,				XK_period, tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
