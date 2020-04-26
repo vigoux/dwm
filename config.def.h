cbfaad46da7136ac816e07a45b853ffc6bc0684b
Thomas Vigouroux <tomvig38@gmail.com>
2020-04-26 15:32:40 +0200

    feat: initial configuration
    Add some keybingings I used in spectrwm.
    Also ad gaps betweem windows.

--- config.def.h
+++ config.def.h
@@ -3,6 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=20" };
@@ -30,7 +35,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
@@ -57,21 +62,37 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "st", "-e", "/bin/fish", NULL };
+
+// Applications
+static const char *browser[] = { "firefox", NULL };
 static const char *todocmd[] = OPENTERMAPP("/bin/calcurse");
 static const char *mailcmd[] = OPENTERMAPP("/bin/neomutt");
+
+// Sounds and music
+#define MASTERVOL(change) { "amixer", "set", "Master", change, NULL }
+#define PLAYER(action) { "playerctl", action, NULL }
 
 static Key keys[] = {
 	/* modifier						key			function		argument */
 	{ MODKEY,						XK_p,		spawn,			{.v = dmenucmd } },
 	{ 0, 							XK_Menu,	spawn,			{.v = dmenucmd } },
+	{ MODKEY,						XK_t,		spawn,			{.v = todocmd } },
+	{ 0, 							XF86XK_Mail,	spawn,		{.v = mailcmd } },
+	{ 0, 							XF86XK_HomePage,	spawn,	{.v = browser } },
+	{ 0, 							XF86XK_AudioRaiseVolume,	spawn,	{.v = MASTERVOL("1db+") } },
+	{ 0, 							XF86XK_AudioLowerVolume,	spawn,	{.v = MASTERVOL("1db-") } },
+	{ 0, 							XF86XK_AudioMute,	spawn,	{.v = MASTERVOL("toggle") } },
+	{ 0, 							XF86XK_AudioPlay,	spawn,	{.v = PLAYER("play-pause") } },
+	{ 0, 							XF86XK_AudioNext,	spawn,	{.v = PLAYER("next") } },
+	{ 0, 							XF86XK_AudioPrev,	spawn,	{.v = PLAYER("previous") } },
 	{ MODKEY,						XK_Return, spawn,			{.v = termcmd } },
 	{ MODKEY,						XK_b,      togglebar,      {0} },
 	{ MODKEY,						XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,						XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,						XK_l,      incnmaster,     {.i = +1 } },
-	{ MODKEY,						XK_h,      incnmaster,     {.i = -1 } },
-	{ MODKEY,						XK_d,      setmfact,       {.f = -0.05} },
-	{ MODKEY,						XK_i,      setmfact,       {.f = +0.05} },
+	{ MODKEY,						XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,						XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,						XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,						XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,				XK_Return, zoom,           {0} },
 	{ MODKEY,						XK_Tab,    view,           {0} },
 	{ MODKEY,						XK_x,      killclient,     {0} },
@@ -86,6 +107,22 @@ static Key keys[] = {
 	{ MODKEY,						XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,				XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,				XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|Mod1Mask,              XK_h,      incrgaps,       {.i = +1 } },
+	{ MODKEY|Mod1Mask,              XK_l,      incrgaps,       {.i = -1 } },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
+	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_y,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|Mod1Mask,              XK_o,      incrohgaps,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
